05c8f4a9a5f24a74b1e46b4af4fafda561549a33
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-09-19 19:39:35 +0800

    [chore] deleted target in math

--- math/target/debug/deps/math-c6af69de0fc27c3c.d
+++
@@ -1,9 +0,0 @@
-/Users/harrypotter/Workspace/RemotWorking/pvz.rs/math/target/debug/deps/math-c6af69de0fc27c3c.rmeta: src/lib.rs src/vec2.rs src/vec3.rs src/vec4.rs src/point.rs
-
-/Users/harrypotter/Workspace/RemotWorking/pvz.rs/math/target/debug/deps/math-c6af69de0fc27c3c.d: src/lib.rs src/vec2.rs src/vec3.rs src/vec4.rs src/point.rs
-
-src/lib.rs:
-src/vec2.rs:
-src/vec3.rs:
-src/vec4.rs:
-src/point.rs:
