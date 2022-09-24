6cf563b844a226f15a0471486651a1e0649e4e60
flyingpig233-cpu <1686079252@qq.com>
2022-09-24 14:31:04 +0800

    [chore] Use bevy framework and add custom mouse functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+use pvz::camera::PVZCameraPlugin;
+use pvz::cursor::PVZCursorPlugin;
+
 fn main() {
-  println!("nm$l")
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "PVZ".to_string(),
+            width: 600.,
+            height: 400.,
+            cursor_visible: false,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PVZCameraPlugin)
+        .add_plugin(PVZCursorPlugin)
+        .run();
 }
